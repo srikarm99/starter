8b21127f610803132b35d8d2836b2bb80503304e
Srikar Malyala <srikarmalyala@gmail.com>
2025-02-20 13:20:34 +0530

    some changes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,72 @@ return {
     end,
   },
 
+{
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xw",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
+    },
+  {
+    "kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		event = "BufReadPost", -- needed for folds to load properly
+  },
+  {
+	"chrisgrieser/nvim-origami",
+	event = "BufReadPost", -- later or on keypress would prevent saving folds
+  lazy = false,
+	opts = true, -- needed even when using default config
+},
+{
+    'saecki/crates.nvim',
+    ft={"rust", "toml"},
+    tag = 'stable',
+    config = function()
+        require('crates').setup{
+          completion = {
+            cmp = {
+              enabled = true
+            },
+            crates = {
+            enabled = true,
+          }
+          }
+        }
+    end,
+},
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
